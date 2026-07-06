93aa60151f66db5682cbcaf41cd0734d99e1b728
adly-248 <adlyfebryan0@gmail.com>
2026-07-06 11:01:22 +0700

    fix: suspense login-admin correct file

--- app/login-admin/page.tsx
+++ app/login-admin/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-
 import { useState, useEffect, Suspense } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
